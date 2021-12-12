e3bfeccbfb0d3216ebdc6a657864aa9c141fdd8f
Derzan Chiang <derzanchiang1800@gmail.com>
2021-12-11 23:11:59 -0800

    add description

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # doodoo
+________
+Don't like using .pop(0) because it looks weird? .pop(0) feels weird to
+type out for a queue?
+
+LOOK NO FURTHER
+
+The doodoo package contains a poop() function which does the same thing as
+.pop(0). BUTTTTTTT since your digestive system is basically a queue (First
+In First Out) it only makes sense to use poop(queue) instead of queue.pop(0)
--- setup.py
+++ setup.py
@@ -2,12 +2,25 @@ from setuptools import setup
 
 setup(
   name="doodoo",
-  version="0.01",
+  version="0.02",
   description="The new pop(0)",
   url="https://github.com/MiTo0o",
   author="MiTo",
   author_email="derzanchiang1800@gmail.com",
   license="MIT",
   packages=["doodoo"],
-  zip_safe=False
+  zip_safe=False,
+  long_description="""
+# doodoo
+________
+Don't like using .pop(0) because it looks weird? .pop(0) feels weird to
+type out for a queue?
+
+LOOK NO FURTHER
+
+The doodoo package contains a poop() function which does the same thing as
+.pop(0). BUTTTTTTT since your digestive system is basically a queue (First
+In First Out) it only makes sense to use poop(queue) instead of queue.pop(0)
+  """,
+  long_description_content_type='text/markdown'
 )
