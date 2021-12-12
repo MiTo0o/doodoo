898aee4b93abe3fdc8a2ae3fe85ffea5d13c9347
Derzan Chiang <derzanchiang1800@gmail.com>
2021-12-11 22:29:07 -0800

    yay published

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name="doodoo",
+  version="0.01",
+  description="The new pop(0)",
+  url="https://github.com/MiTo0o",
+  author="MiTo",
+  author_email="derzanchiang1800@gmail.com",
+  license="MIT",
+  packages=["doodoo"],
+  zip_safe=False
+)
